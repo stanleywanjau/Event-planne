d2f671f5b3d741fb42e06d0aaee3afb68bcbd4d0
Muenishikuku <charlesmueni2@gmail.com>
2024-02-08 20:47:15 +0300

    commit

--- client/src/components/EventGuest.js
+++ client/src/components/EventGuest.js
@@ -8,3 +8,16 @@ const EventGuest = () => {
     fetch(`/event/${event_id}/guest/${guest_id}`, {
       method: 'POST'
     })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to associate guest with event');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Guest added to event:', data.message);
+        // Optionally, you can perform additional actions after successful association
+      })
+      .catch(error => {
+        console.error('Error associating guest with event:', error);
+      });
