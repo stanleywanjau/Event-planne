86d1a466879223357099a1bd13976d5aa16c3f14
Muenishikuku <charlesmueni2@gmail.com>
2024-02-08 15:57:52 +0300

    updated my delete function on Guestlist.js file

--- client/src/components/Guestlist.js
+++ client/src/components/Guestlist.js
@@ -38,3 +38,34 @@ const handleGuestClick = (guestname,guestId) => {
           if (!response.ok) {
             throw new Error('Failed to delete guest');
           }
+
+      // Remove the deleted guest from the guests state
+      setGuests(guests.filter(guest => guest.id !== guestId));
+    })
+    .catch(error => {
+      console.error('Error deleting guest:', error);
+    });
+}
+};
+
+return (
+<div className="guest-list-container">
+  <h2>Guest List</h2>
+  <button onClick={create_guest}>add guest</button>
+  <ul className="guest-list">
+    {guests.map((guest) => (
+      <li key={guest.id} className="guest-item" >
+        <span className="guest-name">Name: {guest.name}</span>
+        <span className="guest-email">Email: {guest.email}</span>
+        <span className="guest-status">Status: {guest.status}</span>
+        <button onClick={() => handleGuestClick(guest.name,guest.id)}>view details</button>
+        <button onClick={() => handleDeleteGuest(guest.id)}>Delete</button>
+      </li>
+    ))}
+  </ul>
+</div>
+);
+};
+
+export default GuestList;      
+  
